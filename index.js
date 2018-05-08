b798635ded3dc18a4a4716649bf67b62c393daa0
Pratama Ryan <30280168+PrtmRyan10@users.noreply.github.com>
2018-05-08 21:03:18 +0700

    add looping

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+ $(document).ready(function() {
+     $("#btnAppend").click(function() {
+         var ya = document.createElement("li");
+         var x = document.getElementById("myText").value;
+         var t = document.createTextNode(x);
+         ya.appendChild(t);
+         document.getElementById("append").appendChild(ya);
+     });
+ });
+ $(document).ready(function() {
+     $("#btnRemove").click(function() {
+         $("#append").empty();
+     });
+ });
+
  $(document).ready(function(){
-      $("#btnAppend").click(function(){
-        var ya = document.createElement("li");
-        var x = document.getElementById("myText").value;
-        var t = document.createTextNode(x);
-        ya.appendChild(t);
-        document.getElementById("append").appendChild(ya);
-      });
-    });
-    $(document).ready(function(){
-      $("#btnRemove").click(function(){
-        $("#append").empty();
-      });
-    });
+      $("#btnCustomLoop").click(function(){
+        var tCL = document.getElementById("txtCustomLoop").value;
+        var nCL = document.getElementById("numberCustomLoop").value;
+        var textcusLoop = "";
+        var i;
+            for (i = 1; i <= nCL; i++) {
+                textcusLoop += i + ". " + tCL + "<br />";
+            }
+         document.getElementById("resCustomLoop").innerHTML = textcusLoop;
+       });
+     });
